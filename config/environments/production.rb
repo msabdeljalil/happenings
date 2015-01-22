0e3922f77db1d684c1b83458d0bbe8f5e73c5b37
msabdeljalil <msabdeljalil@gmail.com>
2015-01-22 09:15:31 -0600

    Added PUBLIC_URL to environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,6 @@ Rails.application.configure do
 
   # Set the production version!
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+    PUBLIC_URL = "http://localhost:3000/"
 end

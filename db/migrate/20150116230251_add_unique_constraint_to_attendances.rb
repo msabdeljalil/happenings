373b41dc1775d0646ec57ac60be65c349f2e928c
msabdeljalil <msabdeljalil@gmail.com>
2015-01-27 09:14:51 -0600

    Removed Old migrations and error file

--- db/migrate/20150116230251_add_unique_constraint_to_attendances.rb
+++
@@ -1,5 +0,0 @@
-class AddUniqueConstraintToAttendances < ActiveRecord::Migration
-  def change
-   execute  "ALTER TABLE attendances ADD CONSTRAINT  uniqueness UNIQUE (user_id, performance_date_id)"
-  end
-end
